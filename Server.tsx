2260eac090e0f64fa6028c90dbecb75be8390960
TreetopFlyer <seth111@gmail.com>
2022-06-21 10:31:38 -0400

    ssr working

--- Server.tsx
+++ Server.tsx
@@ -1,15 +1,46 @@
+import React from "esm/react";
+import ReactDOMServer from "esm/react-dom/server";
 import { serve } from "std/http/server.ts";
 import * as ESBuild from "x/esbuild@v0.14.45/mod.js";
 
-const Headers =
+import App from "./tsx/app.tsx";
+
+const fileConfig = await Deno.readTextFile("./deno.jsonc");
+const Config = JSON.parse(fileConfig);
+Config.importMap = await Deno.readTextFile(Config.importMap);
+
+const ScriptImports = () =>
 {
-    JS:
-    {
-        status: 200,
-        headers: {"content-type": "application/javascript; charset=utf-8"}
-    }
+    return <script type="importmap" dangerouslySetInnerHTML={{__html:Config.importMap}} ></script>;
+};
+const ScriptHydrate = ({client, app}:{client:string, app:JSX.Element}) =>
+{
+    const id = "mount-point";
+    return <>
+    <div id={id}>
+        {app}
+    </div>
+    <script type="module" dangerouslySetInnerHTML={{__html:`
+    import {createElement as h} from "esm/react";
+    import {hydrateRoot} from "esm/react-dom/client";
+    import App from "${client}";
+    hydrateRoot(document.querySelector("#${id}"), h(App));
+    `}} />
+    </>;
 };
 
+const Bedrock = () =>
+{
+    return <html>
+        <head>
+
+        </head>
+        <body>
+            <ScriptImports/>
+            <ScriptHydrate app={<App/>} client="/tsx/App.tsx"/> 
+        </body>
+    </html>;
+};
 
 const Transpile = async (inPath:string):Promise<string|null> =>
 {
@@ -17,21 +48,35 @@ const Transpile = async (inPath:string):Promise<string|null> =>
     {
         const file = await Deno.readTextFile(inPath);
         const code = await ESBuild.transform(file, {loader:"tsx"});
-        console.log(code.code);
         return code.code;
     }
     catch(e)
     {
-        console.log(e);
+        console.log(`Can't find ${e.toString()} for transpiling.`);
         return null;
     }
-}
+};
 
-
+// client
+// static
+// server
 serve(async (inRequest:Request)=>
 {
     const url:URL = new URL(inRequest.url);
-    const code:string|null = await Transpile("./tsx/app.tsx");
-    return new Response(code, Headers.JS);
+
+    if(url.pathname.endsWith(".tsx"))
+    {
+        console.log("transpiling", url.pathname);
+        const code:string|null = await Transpile(url.pathname.substring(1));
+        return new Response(code, { status: 200, headers: {"content-type": "application/javascript; charset=utf-8"} });
+    }
+
+    if(url.pathname.startsWith("/static"))
+    {
+        const file = await Deno.open(url.pathname);
+        return new Response(file.readable);
+    }
+
+    return new Response(ReactDOMServer.renderToString(<Bedrock/>), { status: 200, headers: {"content-type": "text/html"} });
 }
 , {port:3333});
--- tsx/app.tsx
+++ tsx/app.tsx
@@ -2,8 +2,12 @@ import React from "esm/react";
 
 export default ():JSX.Element =>
 {
+    const [countGet, countSet] = React.useState(5);
+
     return <>
         <div>hey</div>
         <p>sup</p>
+        <strong>{countGet}</strong>
+        <button onClick={e=>{countSet(countGet+1)}}>++</button>
     </>;
 };
